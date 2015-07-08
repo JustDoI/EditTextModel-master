df67fbe212aa5705faf5a33751f4c18331523e1e
JustDoI <rxywxsy@163.com>
2015-07-08 17:11:13 +0800

    commit to git

---
+++ app/src/main/java/com/rxy/edittextmodel_master/PhoneTextWatcher.java
@@ -0,0 +1,62 @@
+package com.rxy.edittextmodel_master;
+
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.widget.EditText;
+
+/**
+ * Created by rxy on 15/7/8.
+ * E－mail:rxywxsy@163.com
+ */
+public class PhoneTextWatcher implements TextWatcher {
+
+    private EditText _text;
+
+    public PhoneTextWatcher(EditText _text) {
+        this._text = _text;
+    }
+
+
+    @Override
+    public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+    }
+
+    @Override
+    public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+        if (s == null || s.length() == 0) return;
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < s.length(); i++) {
+            if (i != 3 && i != 8 && s.charAt(i) == ' ') {
+                continue;
+            } else {
+                sb.append(s.charAt(i));
+                if ((sb.length() == 4 || sb.length() == 9) && sb.charAt(sb.length() - 1) != ' ') {
+                    sb.insert(sb.length() - 1, ' ');
+                }
+            }
+        }
+        if (!sb.toString().equals(s.toString())) {
+            int index = start + 1;
+            if (sb.charAt(start) == ' ') {
+                if (before == 0) {
+                    index++;
+                } else {
+                    index--;
+                }
+            } else {
+                if (before == 1) {
+                    index--;
+                }
+            }
+            _text.setText(sb.toString());
+            _text.setSelection(index);
+        }
+
+    }
+
+    @Override
+    public void afterTextChanged(Editable s) {
+
+    }
+}
---
+++ app/src/main/java/com/rxy/edittextmodel_master/Test.java
@@ -0,0 +1,8 @@
+package com.rxy.edittextmodel_master;
+
+/**
+ * Created by rxy on 15/7/8.
+ * E－mail:rxywxsy@163.com
+ */
+public class Test {
+}
